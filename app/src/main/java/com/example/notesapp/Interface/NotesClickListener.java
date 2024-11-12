16255ebaeef5dbf9c7f61048fc0ffd7062fdaae5
Endang <endangsirait2005@gmail.com>
2024-11-12 14:53:51 +0700

    commit

---
+++ app/src/main/java/com/example/notesapp/Interface/NotesClickListener.java
@@ -0,0 +1,11 @@
+package com.example.notesapp.Interface;
+
+import androidx.cardview.widget.CardView;
+
+import com.example.notesapp.Model.Notes;
+
+public interface NotesClickListener {
+    void onClick(Notes notes)
+    void onLongproses(Notes notes, CardView cardView);
+
+}
